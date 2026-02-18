9db7440517eaba9db98b018d607db09b88c30699
AhmedAlSunbati712 <ahmed.m.al.sunbati.27@dartmouth.edu>
2026-02-18 10:15:14 -0500

    user api

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,15 +4,60 @@ import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 const USER_QUERY_KEY = "users";
 
-export const signUp = async (user: Omit<User, "id">) => {
+interface LoginInfo {
+    email: string;
+    password: string;
+}
+export const signUp = async (onSuccess?: () => void, onError?: () => void) => {
     const queryClient = useQueryClient();
     return useMutation({
         mutationFn: async (user: Omit<User, "id">) => {
             const response = await axios.post("/user/signup", user);
             return response.data;
         },
+        onError: (error) => {
+            console.error(error);
+            if (onError) onError;
+        },
         onSuccess: (data) => {
             queryClient.invalidateQueries({ queryKey: [USER_QUERY_KEY] });
+            if (onSuccess) onSuccess();
         },
     });
+}
+
+export const logIn = async (onSuccess?: () => void, onError?: () => void) => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async (loginInfo: LoginInfo) => {
+            const response = await axios.post("/user/login", loginInfo);
+            return response.data;
+        },
+        onError: (error) => {
+            console.error(error);
+            if (onError) onError();
+        },
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({ queryKey: [USER_QUERY_KEY]});
+            if (onSuccess) onSuccess();
+        }
+    })
+}
+
+export const logOut = async (onSuccess?: () => void, onError?: () => void) => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async () => {
+            const response = await axios.post("/user/logout");
+            return response.data;
+        },
+        onError: (error) => {
+            console.error(error);
+            if (onError) onError;
+        },
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({ queryKey: [USER_QUERY_KEY] });
+            if (onSuccess) onSuccess;
+        }
+    })
 }
