fa0f7da1b69d81b4c6284a1450a744fcbb38c5b0
AhmedAlSunbati712 <ahmed.m.al.sunbati.27@dartmouth.edu>
2026-02-18 16:10:36 -0500

    image component

---
+++ src/components/Image/Image.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { getPresignedDownloadUrl } from "@/api/image";
+import { Loader2 } from "lucide-react";
+
+
+interface ImageProps {
+    key: string;
+    className?: string;
+}
+
+
+export default function Image({ key, className }: ImageProps) {
+    const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+    useEffect(() => {
+        getPresignedDownloadUrl(key).then((url) => {
+            setImageUrl(url);
+        });
+    }, [key]);
+
+    return imageUrl ? (
+        <img src={imageUrl} className={className} />
+    ) : (
+        <>
+            <Loader2 className="w-4 h-4 animate-spin" /> Loading...
+        </>
+    );
+}
