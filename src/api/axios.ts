65014027f2c37432c863af47a9c8462c8bac20d0
AhmedAlSunbati712 <ahmed.m.al.sunbati.27@dartmouth.edu>
2026-02-19 17:33:59 -0800

    api url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://172.238.36.250";
 axios.defaults.withCredentials = true;
 axios.defaults.headers['Content-Type'] = 'application/json';
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL || "http://172.238.36.250";
 
 export const ROUTES = {
   HOME: '/',
